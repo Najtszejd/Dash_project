49d4cbdf335df9943e74b57ba97980939ba294f7
Grzegorz Hebda <hebdagrzegorz7@gmail.com>
2025-02-20 23:52:36 +0100

    Ładny, ale naraze ubogi dash

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 from dash import Dash, html, dash_table, dcc
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 from data_api import *
 
-app = Dash()
+app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 
-app.layout = [
-    html.Div(children='--------------------The Most Awesomeness dashboard  in the universe-------------------'),
-    dash_table.DataTable(data=auctions_data_df.to_dict('records'), page_size=10),
-    dash_table.DataTable(data=auctions_info_df.to_dict('records'), page_size=10),
-    dash_table.DataTable(data=distilleries_info_df.to_dict('records'), page_size=10),
-    dash_table.DataTable(data=distilleries_info_df_slug.to_dict('records'), page_size=10),
+app.layout = dbc.Container([
+    html.Div(children='----------------------------------------------------------------- Dane dla poszczególnych aukcji od 2023-01-01 -------------------'),
+    dash_table.DataTable(
+        data=auction_data_df_filter_table.to_dict('records'),
+        page_size=10,
+        sort_action = "native",
+        columns=[
+        {"name": "Nazwa aukcji", "id": "Nazwa aukcji", "type": "text"},
+        {"name": "Liczba pozycji", "id": "Liczba pozycji", "type": "numeric"},
+        {"name": "Wolumen obrotu", "id": "Wolumen obrotu", "type": "numeric"}
+    ],
+        style_table={'overflowX': 'auto'},
+        style_cell={'textAlign': 'left'},
+    ),
+
+    # dash_table.DataTable(data=auctions_info_df.to_dict('records'), page_size=10),
+    # dash_table.DataTable(data=distilleries_info_df.to_dict('records'), page_size=10),
+    # dash_table.DataTable(data=distilleries_info_df_slug.to_dict('records'), page_size=10),
 
 
-    dcc.Graph(figure=px.histogram(auctions_data_df, x='auction_name', y='auction_trading_volume', histfunc='avg')),
-    dcc.Graph(figure=px.histogram(auctions_info_df, x='name', y='buyers_fee', histfunc='avg')),
-    dcc.Graph(figure=px.histogram(distilleries_info_df, x='name', y='whiskybase_votes', histfunc='avg'))
-]
-
+    dcc.Graph(figure=px.line(auctions_data_df[['Data', "Nazwa aukcji","Wolumen obrotu","Liczba pozycji"]], 
+                             x="Data", 
+                             y='Wolumen obrotu', 
+                             color = 'Nazwa aukcji', 
+                             symbol='Nazwa aukcji',
+                             hover_name="Nazwa aukcji",
+                             hover_data={"Liczba pozycji":True},
+                             template="plotly_dark"
+                             ))
+    # dcc.Graph(figure=px.histogram(auctions_info_df, x='name', y='buyers_fee', histfunc='avg')),
+    # dcc.Graph(figure=px.histogram(distilleries_info_df, x='name', y='whiskybase_votes', histfunc='avg'))
+])
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- data_api.py
+++ data_api.py
@@ -9,46 +9,57 @@ distilleries_info = "/distilleries_info/"
 url_1 = f"https://whiskyhunter.net/api{auctions_data}"
 response = requests.get(url_1)
 print(response)
-data_1 = response.json()
+if response.status_code == 200:
+    data_1 = response.json()
+else:
+    print(f"Błąd pobierania danych: {response.status_code}")
+    data_1 = []
+
 auctions_data_df = pd.DataFrame(data_1)
+auctions_data_df = auctions_data_df[auctions_data_df["dt"] > '2023-01-01']
 
+auction_data_df_dict = {
+'dt' : "Data",
+'auction_trading_volume' : "Wolumen obrotu",
+'auction_lots_count' : "Liczba pozycji",
+'auction_name' : "Nazwa aukcji",
+'auction_slug' : "Skrot nazwy aukcji"
+}
 
-auction_slug = [a_slug for a_slug in auctions_data_df["auction_slug"]]
-url_1_1 = f"https://whiskyhunter.net/api/auction_data/{auction_slug}/"
-response = requests.get(url_1_1)
-print(response)
-data_1_1 = response.json()
+auctions_data_df.rename(columns = auction_data_df_dict, inplace = True)
+auctions_data_df["Data"] = pd.to_datetime(auctions_data_df["Data"])
 
-data_auction_slug = pd.DataFrame()
-
-for i in auctions_data_df["auction_slug"]:
-    url_1_1 = f"https://whiskyhunter.net/api/auction_data/{i}/"
-    response = requests.get(url_1_1)
-    data_1_1 = response.json()
-    
-    
-
-
-
+auction_data_df_filter_table = (
+    auctions_data_df.groupby("Nazwa aukcji")[["Liczba pozycji","Wolumen obrotu"]]
+    .sum()
+    .reset_index()
+    )
+auction_data_df_filter_table["Wolumen obrotu"] = pd.to_numeric(auction_data_df_filter_table["Wolumen obrotu"], errors='coerce').round().astype(int)
+# auction_data_df_filter_table["Wolumen obrotu"] = auction_data_df_filter_table["Wolumen obrotu"].round().astype(int)
 
 url_2 = f"https://whiskyhunter.net/api{auctions_info}"
-url_3 = f"https://whiskyhunter.net/api{distilleries_info}"
-url_4 = f"https://whiskyhunter.net/api/distillery_data/{disillery_slug}/"
-
-
-
 response = requests.get(url_2)
 print(response)
-data_2 = response.json()
+if response.status_code == 200:
+    data_2 = response.json()
+else:
+    print(f"Błąd pobierania danych: {response.status_code}")
+    data_2 = []
 auctions_info_df = pd.DataFrame(data_2)
 
+
+url_3 = f"https://whiskyhunter.net/api{distilleries_info}"
 response = requests.get(url_3)
 print(response)
-data_3 = response.json()
+if response.status_code == 200:
+    data_3 = response.json()
+else:
+    print(f"Błąd pobierania danych: {response.status_code}")
+    data_3 = []
 distilleries_info_df = pd.DataFrame(data_3)
 
-
-response = requests.get(url_4)
-print(response)
-data_4 = response.json()
-distilleries_info_df_slug = pd.DataFrame(data_4)
+# url_4 = f"https://whiskyhunter.net/api/distillery_data/{disillery_slug}/"
+# response = requests.get(url_4)
+# print(response)
+# data_4 = response.json()
+# distilleries_info_df_slug = pd.DataFrame(data_4)
